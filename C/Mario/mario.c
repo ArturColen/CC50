d6f12fcdb8dea5544e5ab0885dd9b7e61ce6e86f
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-06-19 10:36:12 -0300

    Update mario.c

--- C/Mario/mario.c
+++ C/Mario/mario.c
@@ -7,7 +7,6 @@ int main(void)
 {
     int height;
 
-    // Ask the user for the height of the pyramid
     do
     {
         height = get_int("Digite a altura da pirâmide: ");
@@ -17,7 +16,6 @@ int main(void)
     buildPyramid(height);
 }
 
-// Create the pyramid according to the height passed
 void buildPyramid(int height)
 {
     int j = 0;
